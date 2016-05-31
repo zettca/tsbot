a74efe56a315d484265b2874f560dc0e98529bcd
Bruno Henriques <corruption1337@gmail.com>
2016-05-31 01:09:57 +0000

    Move APIs and GE ItemList to separate files

---
+++ apis.js
@@ -0,0 +1,170 @@
+const rsStats = ["overall","attack","defence","strength","constitution","ranged","prayer","magic","cooking","woodcutting","fletching","fishing","firemaking","crafting","smithing","mining","herblore","agility","thieving","slayer","farming","runecrafting","hunter","construction","summoning","dungeoneering","divination","invention","bounty_hunter","bounty_hunter_rouges","dominion_tower","crucible","castle_wars","BA_attackers","BA_defenders","BA_collectors","BA_healers","duel_tournaments","mobilising_armies","conquest","fist_of_guthix","GG_resource","GG_athletics","we2","we2","we2","we2","heist_guard","heist_robber","CFP"];
+const rsStatsShort = [["overall", "total", "oa"],["attack", "att", "at"],["defence", "def", "de"],["strength", "str", "st"],["constitution", "hp"],["ranged", "range", "ra", "rang"],["prayer", "pray", "pr"],["magic", "mage", "ma"],["cooking", "cook"],["woodcutting", "wood", "wc", "wo"],["fletching", "fletch", "fl"],["fishing", "fish", "fi"],["firemaking", "fire", "fm"],["crafting", "craft", "cr"],["smithing", "smith", "sm"],["mining", "mine", "mi"],["herblore", "herb", "he"],["agility", "agil", "ag"],["thieving", "thiev", "th"],["slayer", "slay", "sl"],["farming", "farm", "fa"],["runecrafting", "rc", "ru"],["hunter", "hunt", "hu"],["construction", "cons", "construct"],["summoning", "summon", "su"],["dungeoneering", "dungeon", "du", "dung"],["divination", "div", "di"],["invention", "invent", "in"],["bounty_hunter", "bh"],["bounty_hunter_rouges", "bhr"],["dominion_tower", "dt", "dominion"],["crucible"],["castle_wars", "cw"],["BA_attackers", "baa"],["BA_defenders", "bad"],["BA_collectors", "bac"],["BA_healers", "bah"],["duel_tournaments", "duel"],["mobilising_armies", "mob"],["conquest"],["fist_of_guthix", "fog"],["GG_resource", "ggr"],["GG_athletics", "gga"],["we2ac"],["we2bc"],["we2ak"],["we2bk"],["heist_guard", "heistg"],["heist_robber", "heistr"],["CFP"]];
+const askOpts = ["Yes", "No", "Maybe", "Penis is the answer", "There is no answer to that", "Sex will solve that", "Crying will solve that", "Obviously", "Obviously not", "Never", "Everyday", "Keep dreaming...", "Ask somebody dumb instead", "Ask a human instead", "You shouldn't ask such things", "Google it", "The answer is obvious"];
+
+var fs = require('fs');
+var request = require('request');
+var API = function(){};
+
+API.prototype.send = function(cmd, req){
+  var link;
+  
+  switch(cmd){
+    case "a":
+    case "ask":
+      return askZ(req);
+    case "c":
+    case "calc":
+      return calc(req);
+    case "ge":
+    case "price":
+      return rsGEPrice(req);
+    case "rt":
+    case "redtube":
+      link = "http://www.pornhub.com/video/search?search=" + req.replace(/ /g, "+");
+      return "[URL="+link+"]"+link+"[/URL]";
+    case "ph":
+    case "pornhub":
+      link = "http://www.pornhub.com/video/search?search=" + req.replace(/ /g, "+");
+      return "[URL="+link+"]"+link+"[/URL]";
+    case "all":
+    case "stats":
+    case "levels":
+      return rsPlayerStats(req);
+  }
+  
+  if (rsStatIndex(cmd) !== -1) return rsPlayerSkill(cmd, req);
+};
+
+/* ============================== */
+
+
+
+function rsStatIndex(skill){
+  for (var i=0; i<rsStatsShort.length; i++)
+    if (rsStatsShort[i].indexOf(skill) != -1) return i;
+  return -1;
+}
+
+function askZ(req){
+    var value = 0;
+    for (var i=0; i<req.length; i++) value += req.charCodeAt(i);
+    return askOpts[Math.floor(value) % askOpts.length];
+}
+
+function calc(req){
+  var input = req;
+  var ops = { add : '+', sub : '-', div : '/', mlt : '*', mod : '%', exp : '^'};
+  ops.order = [[[ops.mlt], [ops.div], [ops.mod], [ops.exp]], [[ops.add], [ops.sub]]];
+  
+  if (!isNaN(input)) return input;
+  
+  input = input.replace(/k/g, '*1000');
+  input = input.replace(/m/g, '*1000000');
+  input = input.replace(/b/g, '*1000000000');
+  input = input.replace(/t/g, '*1000000000000');
+  input = input.replace(/q/g, '*1000000000000000');
+  input = input.replace(/[^0-9%^*\/()\-+.]/g,'');  // clean up unnecessary characters
+  
+  var output;
+  for(var i=0, n=ops.order.length; i<n; i++ ){
+    // Regular Expression to look for operators between floating numbers or integers
+    var re = new RegExp('(\\d+\\.?\\d*)([\\'+ops.order[i].join('\\')+'])(\\d+\\.?\\d*)');
+    re.lastIndex = 0; // be cautious and reset re start pos
+    
+    // Loop while there is still calculation for level of precedence
+    while(re.test(input)){
+      output = calc_internal(RegExp.$1,RegExp.$2,RegExp.$3);
+      if (isNaN(output) || !isFinite(output)) return output;   // exit early if not a number
+      input = input.replace(re,output);
+    }
+  }
+  
+  return output;
+  
+  function calc_internal(a,op,b){
+    a=a*1; b=b*1;
+    switch(op){
+      case ops.add: return a+b;
+      case ops.sub: return a-b;
+      case ops.div: return a/b;
+      case ops.mlt: return a*b;
+      case ops.mod: return a%b;
+      case ops.exp: return Math.pow(a,b);
+      default: null;
+    }
+  }
+}
+
+
+function rsGEPrice(req){
+  const URL = "http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=";
+  var ID = GENameToID(req);
+  
+  if (!ID) return "[u]" + req + "[/u] is not in my database."; 
+  
+  request({ url: URL + ID, json: true }, function (error, response, body){
+    if (!error && response.statusCode === 200){
+      var name = body.item.name;
+      var price = body.item.current.price;
+      var link = "([URL=http://services.runescape.com/m=itemdb_rs/viewitem.ws?obj="+ID+"]source[/URL])";
+      return name + ": " + price + " " + link;
+    }
+  });
+}
+
+function rsPlayerStats(req){
+  const URL = "http://services.runescape.com/m=hiscore/index_lite.ws?player=";
+
+  request({ url: URL + req, json: true }, function (error, response, body){
+    if (!error && response.statusCode === 200){
+      var skills = {};
+      body.split('\n').forEach(function(item, index){
+        skills[rsStats[index]] = item.split(',');
+      });
+      if (skills){
+        return "\n"+skills["attack"][1]+" Att\t"+skills["constitution"][1]+" HP\t"+skills["mining"][1]+" Mi\n"+skills["strength"][1]+" Str\t"+skills["agility"][1]+" Ag\t"+skills["smithing"][1]+" Sm\n"+skills["defence"][1]+" Def\t"+skills["herblore"][1]+" He\t"+skills["fishing"][1]+" Fi\n"+skills["ranged"][1]+" Ra \t"+skills["thieving"][1]+" Th\t"+skills["cooking"][1]+" Co\n"+skills["prayer"][1]+" Pr \t"+skills["crafting"][1]+" Cr\t"+skills["firemaking"][1]+" FM\n"+skills["magic"][1]+" Ma \t"+skills["fletching"][1]+" Fl\t"+skills["woodcutting"][1]+" WC\n"+skills["runecrafting"][1]+" RC \t"+skills["slayer"][1]+" Sl\t"+skills["farming"][1]+" Fa\n"+skills["construction"][1]+" Co \t"+skills["hunter"][1]+" Hu\t"+skills["summoning"][1]+" Su\n"+skills["dungeoneering"][1]+" Dg\t"+skills["divination"][1]+" Di\t"+skills["overall"][1]+" Total";
+      }
+    }
+  });
+}
+
+function rsPlayerSkill(cmd, req){
+  const URL = "http://services.runescape.com/m=hiscore/index_lite.ws?player=";
+  
+  if (cmd.indexOf("07") != -1){
+    URL = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
+    cmd = cmd.substring(2);
+  }
+  
+  request({ url: URL + req, json: true }, function (error, response, body){
+    if (!error && response.statusCode === 200){
+      var statIndex = rsStatIndex(cmd);
+      var stat = body.split('\n')[statIndex];
+      if (stat){
+        var res = stat.split(',');
+        return rsStats[statIndex] + " level: "+ res[1] + " | " + res[2] + "xp ("+libz.toShortNum(res[2]) + ")";
+      }
+    }
+  });
+}
+
+function GENameToID(itemName){
+  if (!isNaN(itemName)) return itemName;
+  
+  var itemList = JSON.parse(fs.readFileSync("itemlist.json", "utf8"));
+  
+  for (var i=0; i<itemList.length;i++) // exact search
+    if (itemList[i][1].toLowerCase() == itemName.toLowerCase())
+      return itemList[i][0];
+  
+  for (var i=0; i<itemList.length;i++) // contains search
+    if (itemList[i][1].toLowerCase().indexOf(itemName.toLowerCase()) != -1)
+      return itemList[i][0];
+}
+
+String.prototype.capitalize = function(){
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+module.exports = new API();
--- bot.js
+++ bot.js
@@ -1,15 +1,16 @@
 var tsClient = require('node-teamspeak');
-var request = require('request');
 var libz = require('./zettca.js');
+var apis = require('./apis.js');
 
 /* ============================== */
 
-const hour = 60*60*1000;
-const min = 60*1000;
 const sec = 1000;
-const TIME_CHECKUP_TICK = 1 * min;
-const TIME_SPAM_RESET = 20 * min;
-const TIME_AFK_LIMIT = 1 * hour;
+const min = 60*sec;
+const hour = 60*min;
+
+const TIME_CHECKUP_TICK = 10*sec;
+const TIME_SPAM_RESET = 20*min;
+const TIME_AFK_LIMIT = 1*hour;
 
 const SERVER_ADDRESS = process.argv[2];
 const SERVERQUERY_USERNAME = process.argv[3];
@@ -18,88 +19,162 @@ const SERVERQUERY_PASSWORD = process.argv[4];
 /* ============================== */
 
 if (process.argv.length != 5){
-  libz.log("Wrong argument length!");
   libz.log("Correct usage: node bot.js [SERVER-ADDRESS] [SQ-USERNAME] [SQ-PASSWORD]");
   process.exit(1);
 }
 
-process.title = "tsbot";
-console.log();
+process.title = "TSBot";
 libz.log(new Date().toUTCString());
 libz.log("Starting bot service on " + SERVER_ADDRESS + "...");
 
 var botCLID;
 var userList = [];
+var movesList = [];
 var messageList = [];
 var tsBot = new tsClient(SERVER_ADDRESS);  // request sender handler
 
+function sendCmd(cmd, params, callback){
+  if (!cmd) return;
+  var ingoredCmds = ["clientinfo"];
+  
+  tsBot.send(cmd, params, function(error, response, rawResponse){
+    if (error) libz.log("Error running command " + cmd + ". Message: " + error.msg);
+    if (typeof callback === "function"){
+      if (ingoredCmds.indexOf(cmd) == -1) libz.log("Issuing command " + cmd + " with params: " + JSON.stringify(params).replace(/\n/, ""));
+      callback(response);
+    } else if (typeof params === "function"){
+      libz.log("Issuing command " + cmd);
+      params(response);
+    }
+  });
+}
+
 /* ============================== */
 
-function startUp(){
-  tsBot.send("login", { client_login_name: SERVERQUERY_USERNAME, client_login_password: SERVERQUERY_PASSWORD }, function(err, response, rawResponse){
-    if (err){ libz.log("Error logging in: " + err.msg); process.exit(1); }
-    tsBot.send("use", { sid: 1 }, function(err, response, rawResponse){
-      if (err){ libz.log("Error setting default server: " + err.msg); process.exit(1); }
-      
-      libz.log("Logged in on the main server successfully.");
-      
-      // STARTUP
-      tsBot.send("whoami", function(err, response, rawResponse){ botCLID = response.client_id; });
-      tsBot.send("clientupdate", { client_nickname: "ƤlαӵҒϋɳ" });
-      tsBot.send("servernotifyregister", { event: "textchannel" });
-      tsBot.send("servernotifyregister", { event: "textprivate" });
-      tsBot.send("servernotifyregister", { event: "textserver" });
-      tsBot.on("textmessage", handleMessage);
-      
-      checkupMainLoop();
-      setInterval(checkupMainLoop, TIME_CHECKUP_TICK);
-    });
+sendCmd("login", { client_login_name: SERVERQUERY_USERNAME, client_login_password: SERVERQUERY_PASSWORD }, function(res){
+  sendCmd("use", { sid: 1 }, function(res){
+    libz.log("Logged in on the main server successfully.");
+    
+    // STARTUP
+    sendCmd("whoami", function(res){ botCLID = res.client_id; });
+    sendCmd("clientupdate", { client_nickname: "ƤlαӵҒϋɳ" });
+    sendCmd("servernotifyregister", { event: "textchannel" });
+    sendCmd("servernotifyregister", { event: "textprivate" });
+    sendCmd("servernotifyregister", { event: "textserver" });
+    sendCmd("servernotifyregister", { event: "channel", id: 0 });
+    sendCmd("servernotifyregister", { event: "server" });
+    
+    tsBot.on("textmessage", handleMessage);
+    tsBot.on("clientmoved", handleChannelMove);
+    //listenCmd("clientexitview", handleUserExit);
+    //listenCmd("cliententerview", handleUserEnter);
+    
+    setInterval(mainLoop, TIME_CHECKUP_TICK);
   });
+});
+
+/* ============================== */
+
+function mainLoop(){
+  updateUserlist();
+  afkCheckup();
 }
 
-function checkupMainLoop(){
+function updateUserlist(){
   userList = [];
   
-  tsBot.send("clientlist", function(err, response, rawResponse){
-    if (err) libz.log("Error retrieving clientlist: " + err.msg);
-    if (!Array.isArray(response) || response.length < 2) return;
+  sendCmd("clientlist", function(res){
+    if (!Array.isArray(res) || res.length < 2) return;
     
-    response.forEach(function(dude, index){
-      if (dude.client_type == 1) return;
-      tsBot.send("clientinfo", { clid: dude.clid }, function(err, response, rawResponse){
-        if (err){ libz.log("Error retrieving clientinfo: " + err.msg); return; }
-        var user = response;
-        user.clid = dude.clid;
+    for (var i=0; i<res.length; i++){
+      if (res[i].client_type == 1) return;
+      sendCmd("clientinfo", { clid: res[i].clid }, function(res){
+        var user = res;
+        user.clid = res[i].clid;
         userList.push(user);
-        afkCheckup(user);
       });
-    });
+    }
   });
 }
 
-function afkCheckup(user){
+function afkCheckup(){
   var afkCID = 22;
   var ignoredCIDs = [afkCID, 63, 302, 321];
   
-  if (user.client_idle_time > TIME_AFK_LIMIT && ignoredCIDs.indexOf(user.cid) == -1){
-    tsBot.send("clientmove", { clid: user.clid, cid: afkCID }, function(err, response, rawResponse){
-      if (err) libz.log("Error moving client to AFK: " + err.msg);
-      libz.log(user.client_nickname + " was moved to AFK...");
-      tsBot.send("sendtextmessage", { targetmode: 1, target: user.clid, msg: "You were moved to AFK Room for idling for " + libz.timeString(TIME_AFK_LIMIT) + "." });
-    });
+  for (var i=0; i<userList.length; i++){
+    if (userList[i].client_idle_time > TIME_AFK_LIMIT && ignoredCIDs.indexOf(userList[i].cid) === -1){
+      sendCmd("clientmove", { clid: userList[i].clid, cid: afkCID }, function(res){
+        libz.log(userList[i].client_nickname + " was moved to AFK...");
+        sendCmd("sendtextmessage", { targetmode: 1, target: userList[i].clid, msg: "You were moved to AFK Room for idling for " + libz.timeString(TIME_AFK_LIMIT) + "." });
+      });
+    }
   }
 }
 
-function handleMessage(message){
-  if (message.invokerid == botCLID) return; // ignores own messages
-  libz.log("textmessage["+message.targetmode+"] from " + message.invokername + "["+message.invokerid+"]: " + message.msg);
+/* ============================== */
+
+function handleChannelMove(move){
+  const TIME_CLEAR = 2*min;
+  const TIME_MOVESPAM = 6*sec;
+  const TIME_DISABLE_MOVE = 10*min;
   
-  if (message.targetmode == 1 || message.targetmode == 2){  // Private || Channel message
-    processRequests(message.msg, function(output){
-      if (output) tsBot.send("sendtextmessage", { targetmode: message.targetmode, target: message.invokerid, msg: output });
+  if (move.clid == botCLID || (move.invokerid && move.invokerid == botCLID)) return; // ignores own moves
+  
+  move.time = new Date().getTime();
+  if (movesList.length == 0){
+    movesList.push(move);
+    return;
+  }
+  
+  var lastMove = movesList[movesList.length-1];
+  if (lastMove.ctid == move.ctid && lastMove.clid == move.clid) return;
+  
+  console.log(move);
+  movesList.push(move);
+  
+  if (movesList.length >= 4){ // Check last 4 moves
+    var i, thisMove = move;
+    var dudeID = lastMove.clid;
+    
+    for (i=movesList.length-2; i>=movesList.length-4; i--){
+      if (dudeID != movesList[i].clid || movesList[i].invokerid){
+        dudeID = 0;
+        break;
+      }
+    }
+    
+    if (dudeID && thisMove.time - movesList[i+1].time <= TIME_MOVESPAM){
+      var homeID = 9;
+      libz.log(thisMove.clid + " triggered move spam protection...");
+      sendCmd("sendtextmessage", { targetmode: 1, target: thisMove.clid, msg: "Your ability to change channel has been temporairily revoked. Please don't spam channel change next time!" });
+      sendCmd("clientinfo", { clid: thisMove.clid }, function(res){
+        var user = res;
+        sendCmd("servergroupaddclient", { sgid: 24, cldbid: user.client_database_id }, function(res){
+          setTimeout(function(){
+            sendCmd("servergroupdelclient", { sgid: 24, cldbid: user.client_database_id });
+          }, TIME_DISABLE_MOVE);
+        });
+        sendCmd("clientmove", { clid: thisMove.clid, cid: homeID }, function(res){
+          libz.log(user.client_nickname + " was moved to AFK...");
+        });
+      });
+    }
+    
+  }
+  
+  if (movesList.length > 12) movesList.shift();
+}
+
+function handleMessage(msg){
+  if (msg.invokerid == botCLID) return; // ignores own messages
+  libz.log("textmessage["+msg.targetmode+"] from " + msg.invokername + "["+msg.invokerid+"]: " + msg.msg);
+  
+  if (msg.targetmode == 1 || msg.targetmode == 2){  // Private || Channel message
+    processRequests(msg.msg, function(output){
+      if (output) sendCmd("sendtextmessage", { targetmode: msg.targetmode, target: msg.invokerid, msg: output });
     });
-  } else if (message.targetmode == 3){ // Server message
-    handleGlobalMessage(message);
+  } else if (msg.targetmode == 3){ // Server message
+    handleGlobalMessage(msg);
   }
 }
 
@@ -118,127 +193,45 @@ function handleGlobalMessage(message){
   var lastMsg = messageList[messageList.length-2];
   var firstMsg = messageList[0];
   
-  // User sent 2 msg in a row under TIME_REPOST
+  // Server Chat user mute (2 msgs under TIME_REPOST)
   if (thisMsg.invokerid == lastMsg.invokerid && thisMsg.time-lastMsg.time < TIME_REPOST){
-    libz.log(thisMsg.invokername + " triggered spam warning...");
-    tsBot.send("sendtextmessage", { targetmode: 1, target: thisMsg.invokerid, msg: "Your ability to send server messages was temporairily revoked. Please don't talk so fast next time!" });
-    tsBot.send("clientinfo", { clid: thisMsg.invokerid }, function(err, response, rawResponse){
-      tsBot.send("servergroupaddclient", { sgid: 18, cldbid: response.client_database_id });
+    libz.log(thisMsg.invokername + " triggered chat spam protection...");
+    sendCmd("sendtextmessage", { targetmode: 1, target: thisMsg.invokerid, msg: "Your ability to send server messages has been temporairily revoked. Please don't talk so fast next time!" });
+    sendCmd("clientinfo", { clid: thisMsg.invokerid }, function(err, response, rawResponse){
+      sendCmd("servergroupaddclient", { sgid: 18, cldbid: response.client_database_id });
       setTimeout(function(){
-        tsBot.send("servergroupdelclient", { sgid: 18, cldbid: response.client_database_id });
+        sendCmd("servergroupdelclient", { sgid: 18, cldbid: response.client_database_id });
       }, 2*TIME_DISABLE_CHAT);
     });
   }
   
-  // Handle global warnings
+  // Server Chat global mutes
   if (messageList.length >= MAX_MESSAGES && messageList.shift() && thisMsg.time-firstMsg.time < TIME_MUTE){  // Last MAX_MESSAGES messages sent under TIME_MUTE
     libz.log("Major spam detected. Disabling b_client_server_textmessage_send...");
-    tsBot.send("sendtextmessage", { targetmode: 3, target: 1, msg: "Plim, plim, plim... Too much chatting detected! Permissions to talk here were temporairily revoked." });
-    tsBot.send("servergroupaddperm", { sgid: 9, permsid: "b_client_server_textmessage_send", permvalue: 0, permnegated: 0, permskip: 0 });
-    tsBot.send("servergroupaddperm", { sgid: 15, permsid: "b_client_server_textmessage_send", permvalue: 0, permnegated: 0, permskip: 0 });
+    sendCmd("sendtextmessage", { targetmode: 3, target: 1, msg: "Plim, plim, plim... Too much chatting detected! Permissions to talk here were temporairily revoked." });
+    sendCmd("servergroupaddperm", { sgid: 9, permsid: "b_client_server_textmessage_send", permvalue: 0, permnegated: 0, permskip: 0 });
+    sendCmd("servergroupaddperm", { sgid: 15, permsid: "b_client_server_textmessage_send", permvalue: 0, permnegated: 0, permskip: 0 });
     setTimeout(function(){
-      tsBot.send("servergroupaddperm", { sgid: 9, permsid: "b_client_server_textmessage_send", permvalue: 1, permnegated: 0, permskip: 0 });
-      tsBot.send("servergroupaddperm", { sgid: 15, permsid: "b_client_server_textmessage_send", permvalue: 1, permnegated: 0, permskip: 0 });
+      sendCmd("servergroupaddperm", { sgid: 9, permsid: "b_client_server_textmessage_send", permvalue: 1, permnegated: 0, permskip: 0 });
+      sendCmd("servergroupaddperm", { sgid: 15, permsid: "b_client_server_textmessage_send", permvalue: 1, permnegated: 0, permskip: 0 });
     }, TIME_DISABLE_CHAT);
     messageList = [];
   }
 }
 
-function processRequests(msg, callback){
-  var cmd, req, link, URL;
+function processRequests(msg, cbOutput){
   msg = String(msg);
   
   if (msg.charAt(0) != "!" && msg.charAt(0) != "."){ // Not command or question
-    callback(null);
+    cbOutput(null);
     return;
   }
     
   var separator = msg.indexOf(" ");
   
-  cmd = msg.substring(1, separator);
-  req = msg.substring(separator+1);
+  var cmd = msg.substring(1, separator);
+  var req = msg.substring(separator+1);
   
-  var stats = ["overall","attack","defence","strength","constitution","ranged","prayer","magic","cooking","woodcutting","fletching","fishing","firemaking","crafting","smithing","mining","herblore","agility","thieving","slayer","farming","runecrafting","hunter","construction","summoning","dungeoneering","divination","bounty_hunter","bounty_hunter_rouges","dominion_tower","crucible","castle_wars","BA_attackers","BA_defenders","BA_collectors","BA_healers","duel_tournaments","mobilising_armies","conquest","fist_of_guthix","GG_resource","GG_athletics","we2","we2","we2","we2","heist_guard","heist_robber","CFP"];
-
-  
-  if (cmd == "ask" || cmd == "a"){
-    var opts = ["Yes", "No", "Maybe", "Penis is the answer", "There is no answer to that", "Sex will solve that", "Crying will solve that", "Obviously", "Obviously not", "Never", "Everyday", "Keep dreaming...", "Ask somebody dumb instead", "Ask a human instead", "You shouldn't ask such things", "Google it", "The answer is obvious"];
-    var value = 0;
-    for (var i=0; i<msg.length;i++) value += msg.charCodeAt(i);
-    
-    callback(opts[Math.floor(value) % opts.length]);
-    return;
-  } else if (cmd == "calc" || cmd == "c"){
-    var result = libz.calc(req);
-    callback((result < 1000) ? result : result + " ("+libz.toShortNum(result)+")");
-    return;
-  } else if (cmd == "price" || cmd == "ge"){ // GrandExchange Item Search
-    var ID = libz.GENameToID(req);
-
-    if (!ID){ callback("[u]" + req + "[/u] is not in my database."); return; }
-    
-    request({
-      url: ("http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=" + ID),
-      json: true
-    }, function (error, response, body){
-      if (!error && response.statusCode === 200){
-        var name = body.item.name;
-        var price = body.item.current.price;
-        var link = "([URL=http://services.runescape.com/m=itemdb_rs/viewitem.ws?obj="+ID+"]source[/URL])";
-        callback(name + ": " + price + " " + link);
-      }
-    });
-    
-  } else if (cmd == "yt" || cmd == "youtube"){  // YouTube Video Search
-    req = req.replace(/ /g, "+");
-    URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+req+"&key=AIzaSyDh1nE0mDFc9lq6HLqbVV8LzzVMopy9ODY";
-    
-    request({ url: URL, json: true }, function (error, response, body){
-      if (!error && response.statusCode === 200){
-        var link = "https://www.youtube.com/watch?v=" + body.items[0].id.videoId;
-        callback("[URL="+link+"]"+link+"[/URL]");
-      }
-    });
-  } else if (cmd == "rt" || cmd == "redtube"){  // RedTube Video Search
-    link = libz.urlQuery("http://www.redtube.com/?search=", req);
-    callback("[URL="+link+"]"+link+"[/URL]");
-  } else if (cmd == "ph" || cmd == "pornhub"){  // RedTube Video Search
-    req = req.replace(/ /g, "+");
-    link = libz.urlQuery("http://www.pornhub.com/video/search?search=", req);
-    callback("[URL="+link+"]"+link+"[/URL]");
-  } else if (cmd == "stats"){
-    URL = "http://services.runescape.com/m=hiscore/index_lite.ws?player=";
-
-    request({ url: URL + req, json: true }, function (error, response, body){
-      if (!error && response.statusCode === 200){
-        var skills = new Object();
-        body.split('\n').forEach(function(item, index){
-          skills[stats[index]] = item.split(',');
-        });
-        if (skills){
-          var res = "\n"+skills["attack"][1]+" Att\t"+skills["constitution"][1]+" HP\t"+skills["mining"][1]+" Mi\n"+skills["strength"][1]+" Str\t"+skills["agility"][1]+" Ag\t"+skills["smithing"][1]+" Sm\n"+skills["defence"][1]+" Def\t"+skills["herblore"][1]+" He\t"+skills["fishing"][1]+" Fi\n"+skills["ranged"][1]+" Ra \t"+skills["thieving"][1]+" Th\t"+skills["cooking"][1]+" Co\n"+skills["prayer"][1]+" Pr \t"+skills["crafting"][1]+" Cr\t"+skills["firemaking"][1]+" FM\n"+skills["magic"][1]+" Ma \t"+skills["fletching"][1]+" Fl\t"+skills["woodcutting"][1]+" WC\n"+skills["runecrafting"][1]+" RC \t"+skills["slayer"][1]+" Sl\t"+skills["farming"][1]+" Fa\n"+skills["construction"][1]+" Co \t"+skills["hunter"][1]+" Hu\t"+skills["summoning"][1]+" Su\n"+skills["dungeoneering"][1]+" Dg\t"+skills["divination"][1]+" Di\t"+skills["overall"][1]+" Total";
-          callback(res);
-        }
-      }
-    });
-  } else if (libz.RSStatToIndex(cmd) != -1){ // RS Skills
-    URL = "http://services.runescape.com/m=hiscore/index_lite.ws?player=";
-    if (cmd.indexOf("07") != -1){
-      URL = "http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
-      cmd = cmd.substring(2);
-    }
-    
-    request({ url: URL + req, json: true }, function (error, response, body){
-      if (!error && response.statusCode === 200){
-        var statIndex = libz.RSStatToIndex(cmd);
-        var stat = body.split('\n')[statIndex];
-        if (stat){
-          var res = stat.split(',');
-          callback(libz.RSIndexToStat(statIndex) + " level: "+ res[1] + " | " + res[2] + "xp ("+libz.toShortNum(res[2]) + ")");
-        }
-      }
-    });
-  }
+  var oute = apis.get(cmd, req);
+  cbOutput(oute);
 }
-
-startUp();
